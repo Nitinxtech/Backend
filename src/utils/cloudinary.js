dbee5b4115a200a89e6745155548aae7c65f04b0
Nitinxtech <nitinp3006@gmail.com>
2025-01-30 00:52:48 +0530

    registration feature done backend

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,7 @@
-import { v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-import { v2 as cloudinary } from 'cloudinary';
+
 import { log } from "console";
 import { console } from "inspector";
 
@@ -19,7 +19,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("File is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
